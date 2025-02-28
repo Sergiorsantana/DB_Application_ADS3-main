306821c01f6ce1c82f3286c7a3188bbd1cc2bac6
Sergiorssantana <sergiorssantana@hotmail.com>
2025-02-28 11:12:08 -0300

    Configuração da conexão com banco de dados

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
 //console.log('Hello World!')
 
 const express = require('express');
+
+//importar módulo mysql
+const mysql = require('mysql2');
+
+//app é o servidor web
 const app = express();
+
+//Configuração da conexão com o banco de dados
+const conexao = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password:'1234',
+    database: 'projetos'
+});
+
+//teste de conexão com o banco de dados
+conexao.connect(function(error){
+    if(error) throw error;
+    console.log('Conexão com o banco de dados realizada com sucesso!');
+});
+
+
 
 app.get('/', (req, res) => {
     res.write('Hello World!');
